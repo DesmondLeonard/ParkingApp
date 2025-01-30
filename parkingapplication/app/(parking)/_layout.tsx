ede2c59ee995c2ebbacad6610e1aea50afd314a3
Desmond <desle265@student.liu.se>
2025-01-30 14:47:46 +0100

    very basic foundation app

---
+++ parkingapplication/app/(parking)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Slot } from "expo-router";
+
+export default function ParkingLayout() {
+  return <Slot />;
+}
+
+//At a minimum what we need in a Layout file. So basically we are creating new layout with (Parking), and here
+// is the layout file.
---
+++ parkingapplication/app/(tabs)/contact.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Image, Platform } from "react-native";
+
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <IconSymbol
+          size={310}
+          color="#808080"
+          name="chevron.left.forwardslash.chevron.right"
+          style={styles.headerImage}
+        />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Parking-App</ThemedText>
+      </ThemedView>
+      <ThemedText>
+        This app is made so that you don't have to get parking tickets. {"\n"}
+        This will prevent so that you no longer have to pay for parking tickets.{" "}
+        {"\n"}
+        May Allah guide you to the right path. {"\n"}
+      </ThemedText>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
